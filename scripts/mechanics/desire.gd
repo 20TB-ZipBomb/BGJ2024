7b2ac1209cfba6e5aa088336035d572f64c83e95
jake-pauls <jakepauls99@gmail.com>
2024-09-11 00:41:18 -0400

    Added a placeholder enum for storm types and code cleanup/comments

--- scripts/mechanics/desire.gd
+++ scripts/mechanics/desire.gd
@@ -1,7 +1,10 @@
 extends Node
 class_name Desire
 
-enum DesireTypes {
+## General list of desires for each animal.
+## @NOTE: These combine desires that are considered 'general' (thirsty, hungry, etc.)
+## and those that are 'storm-specific' for the time-being.
+enum DesireType {
 	NONE,
 	THIRSTY,
 	INJURED,
@@ -9,45 +12,51 @@ enum DesireTypes {
 	WET,
 	SHOCKED,
 	BURNING,
+	MAX,
 }
 
 ## The animal sprite being updated with desire colors.
 @export var animal_sprite: Sprite3D = null
 ## Maps the desires for each animal to a particular color.
+###
+## In an ideal, forgiving, and graceful world - we could efficiently set these all in the scene, and not in code.
+## https://github.com/godotengine/godot/pull/78656
+## (╯°□°)╯︵ ┻━┻
 @export var desire_to_color_map: Dictionary = {
-	DesireTypes.NONE: Color.WHITE,
-	DesireTypes.THIRSTY: Color.GREEN,
-	DesireTypes.INJURED: Color.PINK,
-	DesireTypes.HUNGRY: Color.SADDLE_BROWN,
-	DesireTypes.WET: Color.BLUE,
-	DesireTypes.SHOCKED: Color.YELLOW,
-	DesireTypes.BURNING: Color.FIREBRICK,
+	DesireType.NONE: Color.WHITE,
+	DesireType.THIRSTY: Color.GREEN,
+	DesireType.INJURED: Color.PINK,
+	DesireType.HUNGRY: Color.SADDLE_BROWN,
+	DesireType.WET: Color.BLUE,
+	DesireType.SHOCKED: Color.YELLOW,
+	DesireType.BURNING: Color.FIREBRICK,
 }
 
-## String representation for the current desire. 
+## String representation for the current desire.
 ## This is readonly, and updating it will have no impact.
 @export var _current_desire_string = ""
-var current_desire: DesireTypes = DesireTypes.NONE
+var current_desire: DesireType = DesireType.NONE
 
 
 func _ready() -> void:
-	var rng = RandomNumberGenerator.new()
-	var desires = desire_to_color_map.keys()
-	var rand = rng.randi_range(0, len(desire_to_color_map) - 1)
-	update_sprite_color_with_desire(desires[rand])
+	roll_new_desire_and_update_sprite()
 
 
 ## Sets the color of the current sprite based on the provided desire
-func update_sprite_color_with_desire(new_desire: DesireTypes) -> void:
+func roll_new_desire_and_update_sprite() -> void:
 	if not animal_sprite:
 		print_debug("The animal sprite is not set, no sprites will have their colors updated")
 		return
-		
-	if not desire_to_color_map.has(new_desire):
-		print_debug("Attempted to set a desire ", new_desire, " but it doesn't have a valid color set")
+
+	var rng = RandomNumberGenerator.new()
+	var random_desire_type = rng.randi_range(0, DesireType.MAX - 1) as DesireType
+	if not desire_to_color_map.has(random_desire_type):
+		print_debug("Attempted to set a desire ", DesireType.keys()[random_desire_type], " but it doesn't have a valid color set")
 		return
-	
-	# Update the current desire and modulate the sprite based on it
-	current_desire = new_desire
-	_current_desire_string = DesireTypes.keys()[current_desire]
+
+	# Update the current desire and its string
+	current_desire = random_desire_type
+	_current_desire_string = DesireType.keys()[current_desire]
+
+	# Modulate the sprite based on the color associated with the desire
 	animal_sprite.modulate = desire_to_color_map[current_desire]
--- scripts/systems/storm_system.gd
+++ scripts/systems/storm_system.gd
@@ -1,6 +1,15 @@
 extends Node
 class_name StormSystem
 
+## Represents the possible types of storms.
+enum StormType {
+	NONE,
+	RAIN,
+	FIRE,
+	LIGHTNING,
+	ALL,
+}
+
 ## The scene spawned for animals.
 ## @TODO: Implement for multiple animals?
 @export_category("Animal")
@@ -10,8 +19,8 @@ class_name StormSystem
 @export var max_number_of_storms_before_max_difficulty: int = 10
 ## Difficulty curve for the number of animals being spawned in each wave.
 ##
-## The x-axis for the curve represents the percentage of rounds completed based 
-## on `max_number_of_storms` and the y-axis represents the number of animals to spawn in.
+## The x-axis for the curve represents the percentage of rounds completed based on `max_number_of_storms` 
+## and the y-axis represents the number of animals to spawn in.
 @export var storm_difficulty_curve: Curve = null
 
 @onready var animal_spline: PathFollow3D = $SpawnPath/SpawnLocation
@@ -20,10 +29,12 @@ class_name StormSystem
 var storm_wave_number: int = 0
 ## Indicates whether the next storm of animals are ready to be spawned
 var next_wave_is_ready: bool = true
+## @TODO: Type for the next storm
+var current_storm_type: StormType = StormType.NONE
 
 
 func _process(_delta: float) -> void:
-	try_spawn_next_wave()
+	try_spawn_next_storm()
 
 
 ## Signal fired whenever the StormTimer ticks.
@@ -31,20 +42,27 @@ func _on_storm_timer_timeout() -> void:
 	# Spin for a random point on the spline
 	animal_spline.progress_ratio = randf()
 
-	# Set the flag to spawn the next wave. We do this because the `update_transform`
-	# for the spline seems to be deferred to it's next tick. If we use the current position
-	# right after setting `progress_ratio` it will always use it's previous position.
+	# Set the flag to spawn the next wave. We do this because the `update_transform` for the spline 
+	# seems to be deferred to it's next tick. If we use the current position right after setting 
+	# `progress_ratio` it will always use it's previous position.
+	#
+	# Reference: https://github.com/godotengine/godot/blob/97ef3c837263099faf02d8ebafd6c77c94d2aaba/scene/3d/path_3d.cpp#L219
 	next_wave_is_ready = true
 
 
-func try_spawn_next_wave() -> void:
+func try_spawn_next_storm() -> void:
 	if not next_wave_is_ready:
 		return
-	
+
 	# Increment the wave number
 	storm_wave_number += 1
 	# Turn off the wave ready flag
 	next_wave_is_ready = false
+
+	# Set the type for this storm
+	# @TODO: Need to determine how this will impact the wave being spawned and the behavior of the animals
+	var rng = RandomNumberGenerator.new()
+	current_storm_type = rng.randi_range(StormType.NONE, StormType.ALL - 1) as StormType
 
 	# Determine the number of cows to spawn using the current wave number,
 	# the maximum number of cows per storm, and the difficulty curve
