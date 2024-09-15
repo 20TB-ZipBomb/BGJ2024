dd601a4c1273b0985307202ba9d7da9c49fd2975
jake-pauls <jakepauls99@gmail.com>
2024-09-15 04:50:08 -0400

    tornado curve regeneration inside bounds of level

--- scripts/mechanics/combo_number.gd
+++ scripts/mechanics/combo_number.gd
@@ -58,7 +58,7 @@ func display_number(value: int, pos: Vector2):
 	# Make the number animate going up by 24 pixels in 0.25s
 	tween.tween_property(number, "position:y", number.position.y - 24, 0.25).set_ease(Tween.EASE_OUT)
 	var _sign = 1 if randi() % 2 else -1
-	tween.tween_property(number, "rotation", _sign * PI / 2, 0.75)
+	tween.tween_property(number, "rotation", _sign * PI / 4, 0.75)
 	# Make it go back down
 	tween.tween_property(number, "position:y", number.position.y, 0.5).set_ease(Tween.EASE_IN).set_delay(0.25)
 	tween.tween_property(number, "scale", Vector2.ZERO, 0.25).set_ease(Tween.EASE_IN).set_delay(0.5)
