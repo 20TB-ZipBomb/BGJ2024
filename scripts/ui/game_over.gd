2b537f70c6458c431a4a292c9569c7d02169a442
EvolHeartrise <evol.heartrise@gmail.com>
2024-09-14 22:21:24 -0700

    attempt game over screen logic

--- scripts/ui/game_over.gd
+++ scripts/ui/game_over.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var return_button: Button = %ReturnButton
+@onready var time_label: Label = %Time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,14 +9,25 @@ func _ready():
 	Globals.game_state_changed.connect(func(new_game_state: Globals.GameState):
 		match new_game_state:
 			Globals.GameState.GAME_OVER:
+				var seconds_elapsed: int = (Time.get_ticks_msec() - Globals.time_began) / 1000
+				var minutes: int = seconds_elapsed / 60
+				var seconds: int = seconds_elapsed % 60
+				
+				var text_string: String = ""
+				if minutes < 10:
+					text_string += "0"
+				text_string += str(minutes)
+				text_string += ":"
+				if seconds < 10:
+					text_string += "0"
+				text_string += str(seconds)
+				time_label.text = "YOUR TIME: " + text_string
 				show()
 			Globals.GameState.GAMEPLAY:
 				hide()
 			Globals.GameState.MENU:
 				hide()
 	)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	return_button.pressed.connect(func():
+		Globals.game_state = Globals.GameState.MENU
+	)
