b5e9f3be2623cb59eb8e4e252026ae1399b43b16
EvolHeartrise <evol.heartrise@gmail.com>
2024-09-14 23:34:51 -0700

    SIMPLIFY

--- scripts/mechanics/pen.gd
+++ scripts/mechanics/pen.gd
@@ -1,25 +1,18 @@
 extends Area3D
 class_name PenArea
 
-## Invisible Leash settings
-@export var min_drag_distance: float = 2
-## Invisible Leash settings
-@export var pull_coefficient: float = 0.2
 @export var desire_type: Desire.DesireType = Desire.DesireType.NONE
 @export var feedback_sound: AudioStreamPlayer
-
-var penned_animals: Array[Animal] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	## When an animal touches the pen area, remove any leashes,
 	## and set their Desire to DesireType.None
 	body_entered.connect(func(body: Node3D):
-		if body is Animal and body not in penned_animals:
+		if body is Animal:
 			var animal: Animal = body
 			if animal.desire.current_desire != desire_type or animal.desire.current_desire == Desire.DesireType.NONE:
 				return
-			penned_animals.append(animal)
 			Leash.unleash(animal)
 			animal.desire.current_desire = Desire.DesireType.NONE
 			Globals.animal_penned_signal.emit(animal)
