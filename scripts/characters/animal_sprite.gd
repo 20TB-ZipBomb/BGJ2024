a0e1b2d19b36e17bc6d19415f61b5765790d9988
EvolHeartrise <evol.heartrise@gmail.com>
2024-09-21 21:00:14 -0700

    optimize animal_sprite.gd flipping logic

--- scripts/characters/animal_sprite.gd
+++ scripts/characters/animal_sprite.gd
@@ -1,13 +1,21 @@
 extends AnimatedSprite3D
 
+var facing_left: bool = true
 
-func _process(_delta):
+func _physics_process(_delta: float) -> void:
+	if not visible:
+		return
+	
 	var animal: CharacterBody3D = owner
 	if animal.velocity.length() < 0.05:
 		play("idle")
 	else:
 		play("walk")
-	if animal.velocity.x < -0.05:
+	if facing_left and animal.velocity.x > 0.05:
 		rotation.y = PI
-	elif animal.velocity.x > 0.05:
+		facing_left = false
+		reset_physics_interpolation()
+	elif not facing_left and animal.velocity.x < -0.05:
 		rotation.y = 0
+		facing_left = true
+		reset_physics_interpolation()
